82c4b83f41ba306d9c350e44a702b3b1759ea27d
cecilDev001 <cecilspencer50@gmail.com>
2024-02-23 10:22:32 +0300

    more updates on client with vite.js configurations

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+// Initialize Express and port
+const app = express();
+const port = 3400 || process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+// App Routes
+
+// Deployment configurations
+
+// App Middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server Error!";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+// Listen to port
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`.cyan.blue.bold);
+});
