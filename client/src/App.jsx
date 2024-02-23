039067d5e1910b4d6d8a4839c1ca6219e5e0b8e5
cecilDev001 <cecilspencer50@gmail.com>
2024-02-23 11:12:19 +0300

    created login, register and home routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
