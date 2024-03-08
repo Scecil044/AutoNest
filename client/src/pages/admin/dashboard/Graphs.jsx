712035416dc545cf3dc48b1a9af451ab77be40e8
cecilDev001 <cecilspencer50@gmail.com>
2024-03-08 12:32:28 +0300

    more changes

--- client/src/pages/admin/dashboard/Graphs.jsx
+++ client/src/pages/admin/dashboard/Graphs.jsx
@@ -1,7 +1,16 @@
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+
+const data = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 export default function Graphs() {
   return (
     <div className="bg-white p-5 shadow-lg hover:scale-105 transition-all duration-500 flex- flex-col">
-      <div className="flex flex-col md:flex-row gap-3">graphs</div>
+
     </div>
   );
 }
--- client/src/pages/admin/dashboard/Numbers.jsx
+++ client/src/pages/admin/dashboard/Numbers.jsx
@@ -1,6 +1,30 @@
-
 export default function Numbers() {
   return (
-    <div className="bg-white p-5 shadow-lg hover:scale-105 transition-all duration-500 flex- flex-col items-start">Numbers</div>
-  )
+    <div className="bg-white p-5 shadow-lg hover:scale-105 transition-all duration-500 flex- flex-col items-start relative">
+      <h1 className="text-darkGreen font-semibold">Vehicles by brand</h1>
+      <div className="flex flex-col gap-1 px-5">
+        <span className="flex justify-between">
+          <h1 className="text-darkGreen font-semibold">BMW </h1>
+          <span>25</span>
+        </span>
+        <span className="flex justify-between">
+          <h1 className="text-darkGreen font-semibold">Subaru </h1>
+          <span>21</span>
+        </span>
+        <span className="flex justify-between">
+          <h1 className="text-darkGreen font-semibold">Honda</h1>
+          <span>251</span>
+        </span>
+        <span className="flex justify-between">
+          <h1 className="text-darkGreen font-semibold">Toyota </h1>
+          <span>250</span>
+        </span>
+        <span className="flex justify-between">
+          <h1 className="text-darkGreen font-semibold">Mercedes</h1>
+          <span>250</span>
+        </span>
+        <div className="absolute py-3 px-4 bg-indigo-800 top-0 -left-4"></div>
+      </div>
+    </div>
+  );
 }
